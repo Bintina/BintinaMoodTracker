1c722d9f5f276cf327fdc4a4238482dc85e93ba7
Bintina <105106238+Bintina@users.noreply.github.com>
2022-07-05 02:00:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/bintinamoodtracker/MainActivity.kt
@@ -0,0 +1,169 @@
+package com.example.bintinamoodtracker
+
+
+import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.math.abs
+
+
+//TODO debug why moods are being skipped
+class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
+
+    lateinit var gestureDetector: GestureDetector
+    var y1: Float = 0.0f
+    var y2: Float = 0.0f
+
+    companion object {
+        const val MIN_DISTANCE = 150
+    }
+
+    var currentMoodScore: Int = 2
+
+    val arrayOfBackgrounds = arrayOf<Int>(
+        R.color.faded_red,
+        R.color.warm_grey,
+        R.color.cornflower_blue_65,
+        R.color.banana_yellow,
+        R.color.warm_grey
+    ).toIntArray()
+
+
+    val arrayOfImages = arrayOf<Int>(
+        R.drawable.smiley_super_happy,
+        R.drawable.smiley_happy,
+        R.drawable.smiley_normal,
+        R.drawable.smiley_disappointed,
+        R.drawable.smiley_sad
+    ).toIntArray()
+
+    private lateinit var background: View
+    private lateinit var moodImage: ImageView
+    private lateinit var noteButton: ImageView
+    private lateinit var historyActivity: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gestureDetector = GestureDetector(this, this)
+
+        background = findViewById(R.id.mood_container)
+        moodImage = findViewById(R.id.emoji_image)
+        noteButton = findViewById(R.id.custom_note)
+        historyActivity = findViewById(R.id.mood_history)
+
+
+        //set MoodImage
+
+        setMood()
+
+        //setMoodViewElements on create 2
+        noteButton.setOnClickListener {
+            //Placeholder to insert a custom note
+        }
+
+        historyActivity.setOnClickListener {
+            //placeholder to go to history screen
+        }
+
+    }
+
+
+    fun setMood() {
+        moodImage.setImageResource(arrayOfImages[currentMoodScore])
+        background.setBackgroundColor(arrayOfBackgrounds[currentMoodScore])
+    }
+
+    //fun SetMoodViewElements() {    }
+
+    //**//GestureDetector
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+
+        gestureDetector.onTouchEvent(event)
+
+
+
+        when (event?.action) {
+
+            0 -> {
+                y1 = event.y
+            }
+
+            1 -> {
+                y2 = event.y
+
+                val valueY: Float = y2 - y1
+
+
+                //TODO restructure so that 91 & 93 aren't required
+
+                if (abs(valueY) > MIN_DISTANCE) {
+                    //top to bottom swipe
+                    if (currentMoodScore > 0) {
+                        if (y2 > y1) {
+                            currentMoodScore -= 1
+                            println("current Mood score is $currentMoodScore")
+                            setMood()
+                        }
+                    }
+                    //bottom to top swipe
+                    if (currentMoodScore < 4) {
+                        if (y2 < y1) {
+                            currentMoodScore += 1
+                            println("current Mood score is $currentMoodScore")
+                            setMood()
+                        }
+                    }
+
+                }
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+
+
+    override fun onDown(e: MotionEvent?): Boolean {
+        //TODO("Not yet implemented")
+        return false
+    }
+
+    override fun onShowPress(e: MotionEvent?) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean {
+        //TODO("Not yet implemented")
+        return false
+    }
+
+    override fun onScroll(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        //TODO("Not yet implemented")
+        return false
+    }
+
+    override fun onLongPress(e: MotionEvent?) {
+        // TODO("Not yet implemented")
+    }
+
+    override fun onFling(
+        e1: MotionEvent?,
+        e2: MotionEvent?,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        // TODO("Not yet implemented")
+        return false
+    }
+
+}
+
+
