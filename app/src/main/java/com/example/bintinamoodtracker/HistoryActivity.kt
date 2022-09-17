0fd1a0b8d1e08848320983992abb1433066ed35e
Bintina <105106238+Bintina@users.noreply.github.com>
2022-09-17 15:23:03 -0700

    MoodClass and MoodEnum updated. Dialog for mood note added

--- app/src/main/java/com/example/bintinamoodtracker/HistoryActivity.kt
+++ app/src/main/java/com/example/bintinamoodtracker/HistoryActivity.kt
@@ -2,10 +2,30 @@ package com.example.bintinamoodtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class HistoryActivity : AppCompatActivity() {
+
+    private lateinit var oneWeekAgoV: View
+    private lateinit var sixDaysAgoV: View
+    private lateinit var fiveDaysAgoV: View
+    private lateinit var fourDaysAgoV: View
+    private lateinit var threeDaysAgoV: View
+    private lateinit var twoDaysAgoV: View
+    private lateinit var oneDayAgoV: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+
+        oneWeekAgoV = findViewById(R.id.seven_days)
+        sixDaysAgoV = findViewById(R.id.six_days)
+        fiveDaysAgoV = findViewById(R.id.five_days)
+        fourDaysAgoV = findViewById(R.id.four_days)
+        threeDaysAgoV = findViewById(R.id.three_days)
+        twoDaysAgoV = findViewById(R.id.yesterday)
+        oneDayAgoV = findViewById(R.id.today)
+
+
     }
 }
--- app/src/main/java/com/example/bintinamoodtracker/MainActivity.kt
+++ app/src/main/java/com/example/bintinamoodtracker/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.bintinamoodtracker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.GestureDetector
+import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.abs
 
-//Save last mood
+//Save last mood in shared preference
 
 class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
 
@@ -49,11 +54,24 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
     private lateinit var noteButton: ImageView
     private lateinit var historyActivity: ImageView
 
+    lateinit var moodSharedPref: SharedPreferences
+    var lastMoodShared: Int = 2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         gestureDetector = GestureDetector(this, this)
+
+        moodSharedPref = getSharedPreferences("LAST_MOOD", MODE_PRIVATE)
+        val editorMoodSharedPref = moodSharedPref.edit()
+
+        editorMoodSharedPref.apply(){
+            putInt("lastMoodString",currentMoodScore)
+            apply()
+        }
+        lastMoodShared = moodSharedPref.getInt("lastMoodString",currentMoodScore).toInt()
+
 
         background = findViewById(R.id.mood_container)
         moodImage = findViewById(R.id.emoji_image)
@@ -68,6 +86,22 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
         //setMoodViewElements on create 2
         noteButton.setOnClickListener {
             //Placeholder to insert a custom note
+            val builder = AlertDialog.Builder(this)
+            val inflater = layoutInflater
+            val dialogLayout = inflater.inflate(R.layout.dialog_layout, null)
+            val moodReasonEt = dialogLayout.findViewById<EditText>(R.id.mood_reason_et)
+
+            with(builder) {
+                setTitle("Make a note")
+                setPositiveButton("Save"){ dialog, which ->
+                    val moodReason = moodReasonEt.text.toString()
+                }
+                setNegativeButton("Cancel"){dialog, which ->
+                    Log.d("Main","Negative button clicked")
+                }
+                setView(dialogLayout)
+                show()
+            }
         }
 
         historyActivity.setOnClickListener {
@@ -81,8 +115,13 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
 
     @RequiresApi(Build.VERSION_CODES.M)
     fun setMood() {
+        moodImage.setImageResource(arrayOfImages[lastMoodShared])
+        background.setBackgroundColor(getColor(arrayOfBackgrounds[lastMoodShared]))
+
         moodImage.setImageResource(arrayOfImages[currentMoodScore])
         background.setBackgroundColor(getColor(arrayOfBackgrounds[currentMoodScore]))
+
+
     }
 
     //fun SetMoodViewElements() {    }
